0ba310fea5147a294c4c5dabe6ab8e2f2b7122b5
RMuthuPrakash <prakashmuthu216@gmail.com>
2022-01-26 07:15:15 +0530

    rewrite the code

--- src/org/tamil/Tamil.java
+++ src/org/tamil/Tamil.java
@@ -4,6 +4,9 @@ public class Tamil {
 	public void tamilLanguage() {
 		System.out.println("ram speak:Tamil");
 	}
+	private void classicalLang() {
+		System.out.println("classicalLang introduced:2004");
+	}
 	
 	
 
